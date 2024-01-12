78654ad952134e4e1ee2f7abf5f4cd55404f9c3f
luannsct <luan.nsct@gmail.com>
2024-01-12 14:19:57 -0300

    :zap: perf: criação utils js

---
+++ src/utils/utils.js
@@ -0,0 +1,9 @@
+const validaStatus = (status) => {
+    const statusArray = [
+        { "ENVIADO": 1 },
+        { "RECEBIDO": 2 },
+        { "ERRO DE ENVIO": 3 }]
+    return (statusArray[0][status] && statusArray[1][status] && statusArray[2][status])
+}
+
+module.exports = { validaStatus }
