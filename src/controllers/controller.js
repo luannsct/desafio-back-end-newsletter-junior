690645ce439ccf73295b8863eb3aa3b5ba50f545
luannsct <luan.nsct@gmail.com>
2024-01-12 14:39:00 -0300

    :sparkles: feat: Criação função listar por evento

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -2,7 +2,8 @@ const { knex } = require("../config/knex");
 
 const atualizarStatusMensagem = async (req, res) => {
     try {
-        const { status } = req.body;
+        let { status } = req.body;
+        status = status.toUpperCase()
         const id = req.idBusca
         const resultado = await knex("sms_message").where({ id }).update({ status }).returning("*");
         return res.status(200).json
@@ -11,4 +12,17 @@ const atualizarStatusMensagem = async (req, res) => {
         return res.status(500).json({ message: "Erro interno!" })
     }
 }
-module.exports = { atualizarStatusMensagem }
+const obterListaMensagensPorStatus = async (req, res) => {
+    let { status } = req.body;
+    status = status.toUpperCase()
+    try {
+        const buscaEnvios = await knex("sms_message").where({ status });
+        if (buscaEnvios.length === 0) {
+            return res.status(404).json({ message: "Nenhuma mensagem encontrada com o status informado." })
+        }
+        return res.status(200).json(buscaEnvios)
+    } catch (error) {
+        return res.status(500).json({ message: "Erro interno!" })
+    }
+}
+module.exports = { atualizarStatusMensagem, obterListaMensagensPorStatus }
