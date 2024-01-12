c0683d0e4274d15db3bc725383554a10751f8016
luannsct <luan.nsct@gmail.com>
2024-01-12 14:39:45 -0300

    :sparkles: feat: Criação rota listar por status

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,8 @@
 const express = require('express')
-const { validarAtualizacaoStatus } = require('../middlewares/middleware')
-const { atualizarStatusMensagem } = require('../controllers/controller')
+const { validarAtualizacaoStatus, validaGetReport } = require('../middlewares/middleware')
+const { atualizarStatusMensagem, obterListaMensagensPorStatus } = require('../controllers/controller')
 const routes = express()
 
 routes.post('/update', validarAtualizacaoStatus, atualizarStatusMensagem)
+routes.get('/reports', validaGetReport, obterListaMensagensPorStatus)
 module.exports = { routes }
