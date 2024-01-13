064d863cbc0f432e552eff25997ad522f0ba17be
luannsct <luan.nsct@gmail.com>
2024-01-13 10:02:48 -0300

    vercel files

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,8 +5,9 @@ const { atualizarStatusMensagem, obterListaMensagensPorStatus, salvarRegistroBan
 const routes = express()
 routes.use(cors())
 
+routes.all('/', (req, res) => { return res.status(200).json({ message: "Olá mundo!" }) })
 routes.post('/insert', validarSalvarResgistro, salvarRegistroBanco)
 routes.post('/update', validarAtualizacaoStatus, atualizarStatusMensagem)
 routes.get('/reports', validaGetReport, obterListaMensagensPorStatus)
-routes.all('/', (req, res) => { return res.status(200).json({ message: "Olá mundo!" }) })
+
 module.exports = { routes }
